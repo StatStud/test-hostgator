23e6ec12884edc8c3bb3b197e9e17fcaabadc5aa
StatStud <63934764+StatStud@users.noreply.github.com>
2023-09-17 07:23:08 -0400

    Create search.php

---
+++ search.php
@@ -0,0 +1,37 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "user_name";
+$password = "password";
+$dbname = "database";
+
+// Create a connection to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the minimum and maximum prices from the form
+$minPrice = $_POST["minPrice"];
+$maxPrice = $_POST["maxPrice"];
+
+// Construct the SQL query
+$sql = "SELECT * FROM menu_items WHERE price BETWEEN $minPrice AND $maxPrice";
+
+// Execute the query
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output the data from the query
+    while ($row = $result->fetch_assoc()) {
+        echo "Product Name: " . $row["item_name"] . " - Price: $" . $row["price"] . "<br>";
+    }
+} else {
+    echo "No products found within the specified price range.";
+}
+
+// Close the database connection
+$conn->close();
+?>
