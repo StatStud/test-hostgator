168eb8fb0fbee9aee9c8a499a37f59f739ed47f0
StatStud <alejandromichel1999@gmail.com>
2023-12-23 19:31:31 -0500

    update Sat Dec 23 19:31:31 EST 2023

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if (!isset($_SESSION['user_id'])) {
     </nav>
 
     <!-- Image spot -->
-    <div class="image-spot" style="margin-top: 75;">
+    <div class="image-spot" style="margin-top: 750;">
         <img src="faces/cover.png">
     </div>
 
