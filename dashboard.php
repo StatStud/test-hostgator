06bce344274f3223faaf7451bde8fd8fd0acb13a
StatStud <alejandromichel1999@gmail.com>
2023-12-25 21:23:06 -0500

    update Mon Dec 25 21:23:06 EST 2023

--- dashboard.php
+++ dashboard.php
@@ -31,59 +31,112 @@ if ($stmt = $conn->prepare($query)) {
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Get the user ID from the session
-  $username = $_SESSION['username'];
 
-  // Initialize variables
-  $update_query = "UPDATE users SET ";
-  $params = array();
-  $types = '';
+  // if we're only modifying the user's attributes
+  if (isset($_POST['modify_attributes'])) {
+      // Get the user ID from the session
+      //$username = $_SESSION['username'];
 
-  // Check and build the query for each field
-  if (isset($_POST['distance']) && $_POST['distance'] !== '') {
-      $update_query .= "distance = ?, ";
-      $params[] = $_POST['distance'];
-      $types .= 'i';
-  }
+      // Initialize variables
+      $update_query = "UPDATE users SET ";
+      $params = array();
+      $types = '';
 
-  if (isset($_POST['languages'])) {
-      $languages = implode(", ", $_POST['languages']);
-      $update_query .= "languages = ?, ";
-      $params[] = $languages;
+      // Check and build the query for each field
+      if (isset($_POST['distance']) && $_POST['distance'] !== '') {
+          $update_query .= "distance = ?, ";
+          $params[] = $_POST['distance'];
+          $types .= 'i';
+      }
+
+      if (isset($_POST['languages'])) {
+          $languages = implode(", ", $_POST['languages']);
+          $update_query .= "languages = ?, ";
+          $params[] = $languages;
+          $types .= 's';
+      }
+
+      if (isset($_POST['hourly_rate']) && $_POST['hourly_rate'] !== '') {
+          $update_query .= "hourly_rate = ?, ";
+          $params[] = $_POST['hourly_rate'];
+          $types .= 'i';
+      }
+
+      if (isset($_POST['bio']) && $_POST['bio'] !== '') {
+        $update_query .= "bio = ?, ";
+        $params[] = $_POST['bio'];
+        $types .= 's';
+      }
+
+      // Remove trailing comma and space from the query
+      $update_query = rtrim($update_query, ", ");
+
+      // Complete the query
+      $update_query .= " WHERE username = ?";
+
+      // Add username parameter and its type
+      $params[] = $username;
       $types .= 's';
+
+      // Prepare and execute the statement
+      if ($stmt = $conn->prepare($update_query)) {
+          $stmt->bind_param($types, ...$params);
+          $stmt->execute();
+          $stmt->close();
+          // Optionally, add a success message here
+      } else {
+          echo "Error updating user settings.";
+      }
   }
 
-  if (isset($_POST['hourly_rate']) && $_POST['hourly_rate'] !== '') {
-      $update_query .= "hourly_rate = ?, ";
-      $params[] = $_POST['hourly_rate'];
-      $types .= 'i';
+  // if we're changing the user's password
+  if (isset($_POST['change_password'])) {
+
+      //$username = $_SESSION['username']; // Get username from the session
+
+      $old_password = $_POST['old_password'];
+      $new_password = $_POST['new_password'];
+      $confirm_password = $_POST['confirm_password'];
+
+      // Validate passwords match
+      if ($new_password !== $confirm_password) {
+          echo "New passwords do not match.";
+          exit();
+      }
+
+      // Fetch the user's current password hash from the database
+      $query = "SELECT password FROM users WHERE username = ?";
+      if ($stmt = $conn->prepare($query)) {
+          $stmt->bind_param("s", $username);
+          $stmt->execute();
+          $stmt->bind_result($stored_password);
+          $stmt->fetch();
+          $stmt->close();
+
+          // Verify the old password matches the stored password
+          if (password_verify($old_password, $stored_password)) {
+              // Hash the new password before updating
+              $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+
+              // Update the user's password in the database
+              $update_query = "UPDATE users SET password = ? WHERE username = ?";
+              if ($stmt = $conn->prepare($update_query)) {
+                  $stmt->bind_param("ss", $hashed_password, $username);
+                  $stmt->execute();
+                  $stmt->close();
+                  echo "Password changed successfully!";
+              } else {
+                  echo "Error updating password.";
+              }
+          } else {
+              echo "Incorrect old password.";
+          }
+      } else {
+          echo "Error fetching password.";
+      }
   }
 
-  if (isset($_POST['bio']) && $_POST['bio'] !== '') {
-    $update_query .= "bio = ?, ";
-    $params[] = $_POST['bio'];
-    $types .= 's';
-  }
 
-  // Remove trailing comma and space from the query
-  $update_query = rtrim($update_query, ", ");
-
-  // Complete the query
-  $update_query .= " WHERE username = ?";
-
-  // Add username parameter and its type
-  $params[] = $username;
-  $types .= 's';
-
-  // Prepare and execute the statement
-  if ($stmt = $conn->prepare($update_query)) {
-      $stmt->bind_param($types, ...$params);
-      $stmt->execute();
-      $stmt->close();
-      // Optionally, add a success message here
-  } else {
-      echo "Error updating user settings.";
-  }
 }
 
 ?>
@@ -184,9 +237,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           <textarea id="bio" name="bio" placeholder="Enter your bio"></textarea><br><br>
 
 
-          <input type="submit" value="Save Changes">
+          <input type="submit" name="modify_attributes" value="Save Changes">
       </form>
   </div>
+
+
+<!-- Password change section -->
+<div class="pwd-change" style="margin: 20px;">
+      <h3>Modify Settings</h3>
+      <form action="" method="POST">
+      <h3>Change Password</h3>
+        <label for="old_password">Old Password:</label>
+        <input type="password" id="old_password" name="old_password" placeholder="Enter old password"><br><br>
+
+        <label for="new_password">New Password:</label>
+        <input type="password" id="new_password" name="new_password" placeholder="Enter new password"><br><br>
+
+        <label for="confirm_password">Confirm Password:</label>
+        <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm new password"><br><br>
+
+        <!-- Submit button for changing password -->
+        <input type="submit" name="change_password" value="Change Password">
+      </form>
+  </div>
+
 
 
 
