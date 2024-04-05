c635e551744c9dbf52e3c367f5ebf5820092cf57
StatStud <alejandromichel1999@gmail.com>
2024-04-05 09:42:47 -0400

    update Fri Apr  5 09:42:47 EDT 2024

--- calculate_square.php
+++ calculate_square.php
@@ -1,10 +1,8 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // run_python_script.php
-    $num = $_GET['num']; // Get input number from AJAX request
-    $command = "python square.py $num";
-    $output = shell_exec($command);
-    echo json_encode(array('result' => $output));
-}
+// run_python_script.php
+$num = $_GET['num']; // Get input number from AJAX request
+$command = "python square.py $num";
+$output = shell_exec($command);
+echo json_encode(array('result' => $output));
 ?>
 
